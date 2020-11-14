c7597f75e7aa09e86b761a84a012cae12d222648
Teemo Tebest <teemo.tebest@gmail.com>
2020-11-14 12:01:58 +0100

    Updated Georgia and North Carolina data for D for R

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -186,7 +186,7 @@ class App extends Component {
               <span className={style.value}>Trump {this.state.r_votes}</span>
             </div>
           </div>
-          <div className={style.clearfix}><label>Source</label><span className={style.value}>DPA</span> <label>Updated</label><span className={style.value}>13.11.2020 4.36 UTC</span></div>
+          <div className={style.clearfix}><label>Source</label><span className={style.value}>DPA</span> <label>Updated</label><span className={style.value}>13.11.2020 19.41 UTC</span></div>
         </div>
       </div>
     );
